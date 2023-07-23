c7bad5044d48b4109537b321efd126c2e0eb63c8
Marc Gilbrecht <marc-gilbrecht@outlook.de>
2023-07-23 05:21:03 +0000

    undo lint changes

--- src/scripts/advanced_overlay.user.js
+++ src/scripts/advanced_overlay.user.js
@@ -11,21 +11,21 @@
 // @grant        none
 // ==/UserScript==
 
-let width = "2000px";
-let height = "1500px";
+let width = '2000px';
+let height = '1500px';
 var button = null;
 
 if (window.top !== window.self) {
-  addEventListener("load", () => {
+  addEventListener('load', () => {
     // ==============================================
-    const STORAGE_KEY = "place-germany-2023-ostate";
+    const STORAGE_KEY = 'place-germany-2023-ostate';
     const OVERLAYS = [
-      ["https://place.army/overlay_target.png", "KLEINE PIXEL"],
-      ["https://place.army/default_target.png", "GROßE PIXEL"],
-      [null, "OVERLAY AUS"],
+      ['https://place.army/overlay_target.png', 'KLEINE PIXEL'],
+      ['https://place.army/default_target.png', 'GROßE PIXEL'],
+      [null, 'OVERLAY AUS'],
     ];
     const getConfig = (text) => {
-      return text + "?" + Date.now();
+      return text + '?' + Date.now();
     };
 
     let oState = {
@@ -40,37 +40,37 @@ if (window.top !== window.self) {
       } catch (e) {}
     }
 
-    const img = document.createElement("img");
-    img.style.pointerEvents = "none";
-    img.style.position = "absolute";
-    img.style.imageRendering = "pixelated";
+    const img = document.createElement('img');
+    img.style.pointerEvents = 'none';
+    img.style.position = 'absolute';
+    img.style.imageRendering = 'pixelated';
     img.src = OVERLAYS[oState.overlayIdx][0];
     img.style.opacity = oState.opacity;
-    img.style.top = "0px";
-    img.style.left = "0px";
+    img.style.top = '0px';
+    img.style.left = '0px';
     img.style.width = width;
     img.style.height = height;
-    img.style.zIndex = "100";
+    img.style.zIndex = '100';
     img.onload = () => {
-      console.log("loaded");
+      console.log('loaded');
       img.style.opacity = oState.opacity / 100;
     };
 
     const mainContainer = document
-      .querySelector("garlic-bread-embed")
-      .shadowRoot.querySelector(".layout");
+      .querySelector('garlic-bread-embed')
+      .shadowRoot.querySelector('.layout');
     const positionContainer = mainContainer
-      .querySelector("garlic-bread-canvas")
-      .shadowRoot.querySelector(".container");
+      .querySelector('garlic-bread-canvas')
+      .shadowRoot.querySelector('.container');
     positionContainer.appendChild(img);
 
     // ==============================================
     // Add buttons to toggle overlay
 
-    const buttonsWrapper = document.createElement("div");
-    buttonsWrapper.style.position = "absolute";
-    buttonsWrapper.style.bottom = "25px";
-    buttonsWrapper.style.right = "25px";
+    const buttonsWrapper = document.createElement('div');
+    buttonsWrapper.style.position = 'absolute';
+    buttonsWrapper.style.bottom = '25px';
+    buttonsWrapper.style.right = '25px';
     mainContainer.appendChild(buttonsWrapper);
 
     const saveState = () => {
@@ -93,7 +93,7 @@ if (window.top !== window.self) {
       if (
         overlayURL === null ||
         overlayURL === undefined ||
-        overlayURL === ""
+        overlayURL === ''
       ) {
         img.style.opacity = 0;
         saveState();
@@ -105,18 +105,18 @@ if (window.top !== window.self) {
     };
 
     const initializeButton = (onClick) => {
-      const button = document.createElement("button");
+      const button = document.createElement('button');
       button.onclick = onClick;
-      button.style.width = "100px";
-      button.style.height = "65px";
-      button.style.backgroundColor = "#555";
-      button.style.color = "white";
-      button.style.border = "var(--pixel-border)";
-      button.style.boxShadow = "var(--pixel-box-shadow)";
-      button.style.fontFamily = "var(--garlic-bread-font-pixel)";
+      button.style.width = '100px';
+      button.style.height = '65px';
+      button.style.backgroundColor = '#555';
+      button.style.color = 'white';
+      button.style.border = 'var(--pixel-border)';
+      button.style.boxShadow = 'var(--pixel-box-shadow)';
+      button.style.fontFamily = 'var(--garlic-bread-font-pixel)';
       // Deutschlandflagge als Hintergrundbild und Farben festlegen
       button.style.backgroundImage =
-        "linear-gradient(to bottom, black, black 33%, red 33%, red 66%, yellow 66%)";
+        'linear-gradient(to bottom, black, black 33%, red 33%, red 66%, yellow 66%)';
       return button;
     };
 
@@ -136,30 +136,30 @@ if (window.top !== window.self) {
     };
 
     const renderOpacitySlider = (val, onChange) => {
-      const opacityWrapper = document.createElement("div");
-      opacityWrapper.style.width = "100px";
-      opacityWrapper.style.height = "45px";
-      opacityWrapper.style.backgroundColor = "#555";
-      opacityWrapper.style.color = "white";
-      opacityWrapper.style.border = "var(--pixel-border)";
-      opacityWrapper.style.boxShadow = "var(--pixel-box-shadow)";
-      opacityWrapper.style.fontFamily = "var(--garlic-bread-font-pixel)";
-      opacityWrapper.style.marginTop = "15px";
-      opacityWrapper.style.textAlign = "center";
-      opacityWrapper.innerText = "Transparenz";
+      const opacityWrapper = document.createElement('div');
+      opacityWrapper.style.width = '100px';
+      opacityWrapper.style.height = '45px';
+      opacityWrapper.style.backgroundColor = '#555';
+      opacityWrapper.style.color = 'white';
+      opacityWrapper.style.border = 'var(--pixel-border)';
+      opacityWrapper.style.boxShadow = 'var(--pixel-box-shadow)';
+      opacityWrapper.style.fontFamily = 'var(--garlic-bread-font-pixel)';
+      opacityWrapper.style.marginTop = '15px';
+      opacityWrapper.style.textAlign = 'center';
+      opacityWrapper.innerText = 'Transparenz';
 
-      const opacitySlider = document.createElement("input");
-      opacitySlider.type = "range";
+      const opacitySlider = document.createElement('input');
+      opacitySlider.type = 'range';
       opacitySlider.min = 0;
       opacitySlider.max = 100;
       opacitySlider.value = val;
-      opacitySlider.style.webkitAppearance = "none";
-      opacitySlider.style.appearance = "none";
-      opacitySlider.style.height = "15px";
-      opacitySlider.style.width = "95px";
-      opacitySlider.style.borderRadius = "5px";
-      opacitySlider.style.background = "#d3d3d3";
-      opacitySlider.style.outline = "none";
+      opacitySlider.style.webkitAppearance = 'none';
+      opacitySlider.style.appearance = 'none';
+      opacitySlider.style.height = '15px';
+      opacitySlider.style.width = '95px';
+      opacitySlider.style.borderRadius = '5px';
+      opacitySlider.style.background = '#d3d3d3';
+      opacitySlider.style.outline = 'none';
       opacitySlider.oninput = onChange;
 
       opacityWrapper.appendChild(opacitySlider);
